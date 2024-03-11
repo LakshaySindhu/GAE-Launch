f0eb740c026afb68209021ee980ad156e4d42fff
LakshaySindhu <137389809+LakshaySindhu@users.noreply.github.com>
2024-03-11 11:52:01 +0530

    Create code.cpp

---
+++ code.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+  cout<<"Running Application using Google App Engine"<<endl;
+  cout<<"Application Running on Cloud Console"<<endl;
+  cout<<"Cloud Computing Experiment"<<endl;
+  cout<<"LAB MST Experiment"<<endl;
+  cout<<"Name-Lakshay Sindhu"<<endl;
+  cout<<"UID-21BCS10420"<<endl;
+
+  return 0;
+}
